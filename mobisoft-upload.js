74331aed917e32ca5eab05b01d4b7d34f1397946
elsteno <elsteno@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-25 16:00:04 +0000

    Upload file code correction
    git-svn-id: https://plugins.svn.wordpress.org/wp-soononline-page/trunk@673081 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mobisoft-upload.js
+++ mobisoft-upload.js
@@ -1,3 +1,30 @@
-jQuery(document).ready(function($){
-    $('.mobicolor-field').wpColorPicker();
+jQuery(document).ready(function() {
+  
+    var formfield;
+
+    jQuery('.mobisoft-upload-button').click(function() {
+        formfield = jQuery(this).prev('input');
+        tb_show('','media-upload.php?TB_iframe=true');  
+        return false;  
+    });
+    /* The above code Developed by oneTarek http://onetarek.com
+	    Many thanks
+    */ 
+    window.old_tb_remove = window.tb_remove;
+    window.tb_remove = function() {
+        window.old_tb_remove(); 
+        formfield=null;
+    };
+  
+    window.original_send_to_editor = window.send_to_editor;
+    window.send_to_editor = function(html){
+        if (formfield) {
+            fileurl = jQuery('img',html).attr('src');
+            jQuery(formfield).val(fileurl);
+            tb_remove();
+        } else {
+            window.original_send_to_editor(html);
+        }
+    };
+  
 });
